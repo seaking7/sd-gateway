771915b42672609ccc3d164a31c5e438f9bcff6e
Kim Taekyung <seaking7@gmail.com>
2021-06-04 10:44:07 +0900

    Initial commit

---
+++ src/main/java/com/uplus/sdgateway/SdGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.uplus.sdgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uplus/sdgateway/SdGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplus.sdgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
